88abeec34762ab828e04c83c340440ec2196b49a
Lúcia Wolf Schafer <bl1nd4ng3l@gmail.com>
2018-11-08 12:40:45 -0200

    finished pause mechanics and UI

--- Werewolf/GameScenes/Game.gd
+++ Werewolf/GameScenes/Game.gd
@@ -10,5 +10,34 @@ func _process(delta):
 
 func pause():
 	if $CanvasLayer/UI/PauseButton.pressed:
-		get_tree().paused = !get_tree().paused
+		get_tree().paused = true
+		$CanvasLayer/UI/PauseScreen.set_visible(true)
+		$CanvasLayer/UI/PauseButton.set_visible(false)
+	if $CanvasLayer/UI/PauseScreen/ResumeButton.pressed:
+		$CanvasLayer/UI/PauseScreen.set_visible(false)
+		$CanvasLayer/UI/PauseButton.set_visible(true)
+		get_tree().paused = false
+	if $CanvasLayer/UI/PauseScreen/OptionsButton.pressed:
+		$CanvasLayer/UI/PauseScreen/ResumeButton.set_visible(false)
+		$CanvasLayer/UI/PauseScreen/OptionsButton.set_visible(false)
+		$CanvasLayer/UI/PauseScreen/MainMenuButton.set_visible(false)
+		$CanvasLayer/UI/PauseScreen/OptionsScreen.set_visible(true)
+	if $CanvasLayer/UI/PauseScreen/OptionsScreen/Back.pressed:
+		$CanvasLayer/UI/PauseScreen/ResumeButton.set_visible(true)
+		$CanvasLayer/UI/PauseScreen/OptionsButton.set_visible(true)
+		$CanvasLayer/UI/PauseScreen/MainMenuButton.set_visible(true)
+		$CanvasLayer/UI/PauseScreen/OptionsScreen.set_visible(false)
+	if $CanvasLayer/UI/PauseScreen/MainMenuButton.pressed:
+		$CanvasLayer/UI/PauseScreen/ResumeButton.set_visible(false)
+		$CanvasLayer/UI/PauseScreen/OptionsButton.set_visible(false)
+		$CanvasLayer/UI/PauseScreen/MainMenuButton.set_visible(false)
+		$CanvasLayer/UI/PauseScreen/MainMenuScreen.set_visible(true)
+	if $CanvasLayer/UI/PauseScreen/MainMenuScreen/NoButton.pressed:
+		$CanvasLayer/UI/PauseScreen/ResumeButton.set_visible(true)
+		$CanvasLayer/UI/PauseScreen/OptionsButton.set_visible(true)
+		$CanvasLayer/UI/PauseScreen/MainMenuButton.set_visible(true)
+		$CanvasLayer/UI/PauseScreen/MainMenuScreen.set_visible(false)
+	if $CanvasLayer/UI/PauseScreen/MainMenuScreen/YesButton.pressed:
+		get_tree().paused = false
+		get_tree().change_scene("res://GameScenes/MainMenu.tscn")
 	pass
